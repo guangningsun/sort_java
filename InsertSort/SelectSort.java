3aabf99518bb4ff6032cb85d8f0106689172606b
guangningsun <guangningsun@sohu-inc.com>
2015-05-27 15:26:00 +0800

    增加选择排序算法

--- InsertSort/SelectSort.java
+++ InsertSort/SelectSort.java
@@ -1,4 +1,20 @@
 
 public class SelectSort {
-
+	public int [] selectSort(int []list){
+		int min=0;
+		for(int i=0;i<list.length;i++){
+			min = i;
+			for(int j=i+1;j<list.length;j++){
+				if(list[min]>list[j]){
+					min =j;
+				}
+			}
+			if(min!=i){
+				int tmp = list[i];
+				list[i]= list[min];
+				list[min]=tmp;
+			}
+		}
+		return list;
+	}
 }
